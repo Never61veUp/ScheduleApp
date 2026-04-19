abd936be1a92796d7356326ad6a40b3cc33ccffc
Ke4aMbiT <antonaganov07@gmail.com>
2026-04-19 22:06:30 +0500

    Добавил base: './', чтобы билд не выдавал белый экран в ТГ

--- miniapp/vite.config.ts
+++ miniapp/vite.config.ts
@@ -3,14 +3,17 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ 
+      babelHelpers: 'bundled',
+      presets: [reactCompilerPreset()] 
+    })
   ],
-    server:{
-        allowedHosts: ['sheduleminiapp.ru.tuna.am']
-    }
+  base: './',
+  server: {
+    allowedHosts: ['sheduleminiapp.ru.tuna.am']
+  }
 })
